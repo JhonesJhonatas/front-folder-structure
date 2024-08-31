71090755cff1eda280892d1fef71771fcc7ff972
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-08-31 11:47:51 -0300

    feat: implement translation on home and navbar

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -2,20 +2,27 @@ import { GoComment, GoFileDirectory, GoHome, GoInfo } from 'react-icons/go'
 
 import { NavItem, NavItems } from './nav-items';
 import { LanguageSwitch } from './language-switch';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
-const navItems: NavItem[] = [
-  { label: 'Home', href: '/', icon: GoHome },
-  { label: 'Explanation', href: '/explanation', icon: GoInfo },
-  { label: 'Discussion', href: '/discussion', icon: GoComment },
-]
 
 export function NavBar() {
+  const { t } = useTranslation()
+
+  const navItems: NavItem[] = useMemo(() => {
+    return [
+      { label: t('navBar.items.home'), href: '/', icon: GoHome },
+      { label: t('navBar.items.explanation'), href: '/explanation', icon: GoInfo },
+      { label: t('navBar.items.discution'), href: '/discussion', icon: GoComment },
+    ]
+  },[t])
+
   return (
     <header className="p-4 bg-zinc-800 flex items-center justify-center">
       <div className="flex items-center justify-between w-8/12">
         <div className="flex items-center gap-2">
           <GoFileDirectory size={24} />
-          <span className="font-bold" >FrontEnd Folder Architeture</span>
+          <span className="font-bold" >{t('navBar.title')}</span>
         </div>
         <div className='flex items-center gap-2'>
           <NavItems items={navItems} />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,16 @@
 import codePrint from '@/assets/code-print.png'
 
+import { useTranslation } from 'react-i18next'
+
 export function Home() {
+  const {t} = useTranslation()
   return (
     <div className="flex flex-col gap-4">
       <section className='flex justify-between'>
         <div className='flex flex-col gap-8 justify-center'>
-          <h1 className='text-8xl font-bold'>FrontEnd Folder Architeture</h1>
+          <h1 className='text-8xl font-bold'>{t('home.title')}</h1>
           <p className='text-4xl font-thin'>
-            This project is a example of a scalable FrontEnd Folder Architeture.
+            {t('home.description')}
           </p>
         </div>
         <img src={codePrint} className='w-6/12 rounded shadow-xl' />
